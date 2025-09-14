82bdc48e0e1bb22a48b5e2b770e9fce77c08d927
ab1355 <144258346+ab1355@users.noreply.github.com>
2025-09-13 21:46:14 -0400

    Update index.ts

--- questflow/task-manager/libs/agents-core/src/index.ts
+++ questflow/task-manager/libs/agents-core/src/index.ts
@@ -2,7 +2,9 @@ export * from "./logger.js";
 export * from "./specs.js";
 export * from "./akash.js";
 export * from "./ai.js";
+export * from "./config.js";
 
 import * as mocks from "./__mocks__/index.js";
 export { mocks };
+
 
